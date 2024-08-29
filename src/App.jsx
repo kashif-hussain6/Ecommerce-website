91d6b1f2ad6cd2855e9a3ac3a471dfeaa9fff460
kashif-aly6466 <159264621+kashif-aly6466@users.noreply.github.com>
2024-08-28 23:50:39 -0700

    make the dots in the slider of hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
-// // import Hero from "./components/Hero/Hero";
+import Hero from "./components/Hero/Hero";
 // // import Category from "./components/Category/Category";
 // // import Category2 from "./components/Category/Category2";
 // // import Services from "./components/Services/Services";
-import Banner from "./components/Banner/Banner";
+// import Banner from "./components/Banner/Banner";
 // // import Partners from "./components/Partners/Partners.jsx";
 
 // import headphone from "./assets/hero/headphone.png";
@@ -61,7 +61,7 @@ const App = () => {
   return (
     <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden">
       <Navbar />
-      <Banner />
+      <Hero />
       {/* <Hero handleOrderPopup={handleOrderPopup} />
       <Category />
       <Category2 />
