11ed183de6759e55e5c02ee2877cf861b60af920
kashif-aly6466 <159264621+kashif-aly6466@users.noreply.github.com>
2024-08-30 08:06:12 -0700

    manage the colours and thems

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import Image1 from "../../assets/category/earphone.png";
 import Image2 from "../../assets/category/watch.png";
@@ -10,8 +9,8 @@ const Category = () => {
     <div className="py-8">
       <div className="container">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-          {/* first col */}
-          <div className="py-10 pl-5 bg-gradient-to-br from-black/90 to-black/70 text-white rounded-3xl relative h-[320px] flex items-end">
+          {/* First Col */}
+          <div className="py-10 pl-5 bg-gradient-to-br from-black/90 to-black/70 text-white rounded-3xl relative h-[320px] flex items-end transition-transform duration-300 hover:scale-105">
             <div>
               <div className="mb-4">
                 <p className="mb-[2px] text-gray-400">Enjoy</p>
@@ -28,8 +27,9 @@ const Category = () => {
             </div>
             <img src={Image1} alt="" className="w-[320px] absolute bottom-0" />
           </div>
-          {/* second col */}
-          <div className="py-10 pl-5 bg-gradient-to-br from-brandYellow to-brandYellow/90 text-white rounded-3xl relative h-[320px] flex items-end">
+
+          {/* Second Col */}
+          <div className="py-10 pl-5 bg-gradient-to-br from-brandYellow to-brandYellow/90 text-white rounded-3xl relative h-[320px] flex items-end transition-transform duration-300 hover:scale-105">
             <div>
               <div className="mb-4">
                 <p className="mb-[2px] text-white">Enjoy</p>
@@ -50,8 +50,9 @@ const Category = () => {
               className="w-[320px] absolute -right-4 lg:top-[40px]"
             />
           </div>
-          {/* third col */}
-          <div className="sm:col-span-2 py-10 pl-5 bg-gradient-to-br from-primary to-primary/90 text-white rounded-3xl relative h-[320px] flex items-end">
+
+          {/* Third Col */}
+          <div className="sm:col-span-2 py-10 pl-5 bg-gradient-to-br from-primary to-primary/90 text-white rounded-3xl relative h-[320px] flex items-end transition-transform duration-300 hover:scale-105">
             <div>
               <div className="mb-4">
                 <p className="mb-[2px] text-white">Enjoy</p>
@@ -79,4 +80,3 @@ const Category = () => {
 };
 
 export default Category;
-
