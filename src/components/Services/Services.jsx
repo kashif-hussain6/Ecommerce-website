ffeba8d1b5df0b93d0b45c3b94e99c24daf99a7c
kashif-aly6466 <159264621+kashif-aly6466@users.noreply.github.com>
2024-08-31 09:05:20 -0700

    make the services section here

---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {
+  FaCarSide,
+  FaHeadphonesAlt,
+  FaWallet,
+  FaCheckCircle,
+} from "react-icons/fa";
+
+const ServiceData = [
+  {
+    id: 1,
+    icon: <FaCarSide className="text-4xl md:text-5xl text-primary" />,
+    title: "Free Shipping",
+    description: "Free Shipping On All Order",
+  },
+  {
+    id: 2,
+    icon: <FaCheckCircle className="text-4xl md:text-5xl text-primary" />,
+    title: "Safe Money ",
+    description: "30 Days Money Back",
+  },
+
+];
+
+const Services = () => {
+  return (
+    <div>
+      <div className="container my-14 md:my-20">
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 gap-y-8">
+          {ServiceData.map((data) => (
+           
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
