f8bb26680d2d7a28a1a311f545cb4570c07397c1
kashif-aly6466 <159264621+kashif-aly6466@users.noreply.github.com>
2024-08-31 09:12:08 -0700

    add some more services here

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -19,6 +19,18 @@ const ServiceData = [
     title: "Safe Money ",
     description: "30 Days Money Back",
   },
+  {
+    id: 1,
+    icon: <FaHeadphonesAlt className="text-4xl md:text-5xl text-primary" />,
+    title: "Free Shipping",
+    description: "Free Shipping On All Order",
+  },
+  {
+    id: 2,
+    icon: <FaWallet className="text-4xl md:text-5xl text-primary" />,
+    title: "Safe Money ",
+    description: "30 Days Money Back",
+  },
 
 ];
 
@@ -28,6 +40,13 @@ const Services = () => {
       <div className="container my-14 md:my-20">
         <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 gap-y-8">
           {ServiceData.map((data) => (
+             <div className="flex flex-col items-start sm:flex-row gap-4">
+             {data.icon}
+             <div>
+               <h1 className="lg:text-xl font-bold">{data.title}</h1>
+               <h1 className="text-gray-400 text-sm">{data.description}</h1>
+             </div>
+           </div>
            
           ))}
         </div>
