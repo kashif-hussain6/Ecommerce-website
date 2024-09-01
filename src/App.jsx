489017989bf73352f308a9cc877644b199ef8d5a
kashif-aly6466 <159264621+kashif-aly6466@users.noreply.github.com>
2024-09-01 11:26:45 -0700

    manage the animation in the banner section

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,12 @@ import Category from "./components/Category/Category";
 import Category2 from "./components/Category/Category2";
 import Services from "./components/Services/Services";
 import Banner from "./components/Banner/Banner";
-// // import Partners from "./components/Partners/Partners.jsx";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-// import headphone from "./assets/hero/headphone.png";
+// Import images
+import headphone from "./assets/hero/headphone.png";
 // import smartwatch2 from "./assets/category/smartwatch2-removebg-preview.png";
-
-// // import Products from "./components/Products/Products";
-// // import Blogs from "./components/Blogs/Blogs";
-// // import Footer from "./components/Footer/Footer.jsx";
-// import Popup from "./components/Popup/Popup.jsx";
-// import AOS from "aos";
-// import "aos/dist/aos.css";
 
 const BannerData = {
   discount: "30% OFF",
@@ -29,34 +24,28 @@ const BannerData = {
   bgColor: "#f42c37",
 };
 
-const BannerData2 = {
-  discount: "30% OFF",
-  title: "Happy Hours",
-  date: "14 Jan to 28 Jan",
-  image: smartwatch2,
-  title2: "Smart Solo",
-  title3: "Winter Sale",
-  title4:
-    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque reiciendis",
-  bgColor: "#2dcc6f",
-};
+// const BannerData2 = {
+//   discount: "30% OFF",
+//   title: "Happy Hours",
+//   date: "14 Jan to 28 Jan",
+//   image: smartwatch2,
+//   title2: "Smart Solo",
+//   title3: "Winter Sale",
+//   title4:
+//     "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque reiciendis",
+//   bgColor: "#2dcc6f",
+// };
 
-// const App = () => {
-  // const [orderPopup, setOrderPopup] = React.useState(false);
-
-  // const handleOrderPopup = () => {
-  //   setOrderPopup(!orderPopup);
-  // };
-
-  // React.useEffect(() => {
-  //   AOS.init({
-  //     duration: 800,
-  //     easing: "ease-in-sine",
-  //     delay: 100,
-  //     offset: 100,
-  //   });
-  //   AOS.refresh();
-  // }, []);
+const App = () => {
+  React.useEffect(() => {
+    AOS.init({
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+      offset: 100,
+    });
+    AOS.refresh();
+  }, []);
 
   return (
     <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden">
@@ -65,19 +54,8 @@ const BannerData2 = {
       <Category />
       <Category2 />
       <Services />
-      
-       <Banner data={BannerData2} />
+      {/* <Banner data={BannerData2} /> */}
       <Banner data={BannerData} />
-
-      {/* <Hero handleOrderPopup={handleOrderPopup} />
-      
-    
-      <Products />
-      
-      <Blogs />
-      <Partners />
-      <Footer />
-      <Popup orderPopup={orderPopup} handleOrderPopup={handleOrderPopup} /> */}
     </div>
   );
 };
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -8,7 +8,7 @@ const Banner = ({ data }) => {
           style={{ backgroundColor: data.bgColor }}
           className="grid grid-cols-1 md:grid-cols-3 gap-6 items-center text-white rounded-3xl"
         >
-          {/* first col */}
+          {/* First Column */}
           <div className="p-6 sm:p-8">
             <p data-aos="slide-right" className="text-sm">
               {data.discount}
@@ -17,22 +17,21 @@ const Banner = ({ data }) => {
               data-aos="zoom-out"
               className="uppercase text-4xl lg:text-7xl font-bold"
             >
-              {" "}
               {data.title}
             </h1>
             <p data-aos="fade-up" className="text-sm">
               {data.date}
             </p>
           </div>
-          {/* second col */}
+          {/* Second Column */}
           <div data-aos="zoom-in" className="h-full flex items-center">
             <img
               src={data.image}
-              alt=""
+              alt="Promotional product"
               className="scale-125 w-[250px] md:w-[340px] mx-auto drop-shadow-[-10px_10px_12px_rgba(0,0,0,.6)] object-cover"
             />
           </div>
-          {/* third col */}
+          {/* Third Column */}
           <div className="flex flex-col justify-center gap-4 p-6 sm:p-8">
             <p data-aos="zoom-out" className="font-bold text-xl">
               {data.title2}
