d48f712873375c6b7f02da7286f857334288d6ff
kashif-aly6466 <159264621+kashif-aly6466@users.noreply.github.com>
2024-09-01 10:54:45 -0700

    manage the data of the banner section and add transtion

--- src/App.jsx
+++ src/App.jsx
@@ -17,31 +17,31 @@ import Banner from "./components/Banner/Banner";
 // import AOS from "aos";
 // import "aos/dist/aos.css";
 
-// const BannerData = {
-//   discount: "30% OFF",
-//   title: "Fine Smile",
-//   date: "10 Jan to 28 Jan",
-//   image: headphone,
-//   title2: "Air Solo Bass",
-//   title3: "Winter Sale",
-//   title4:
-//     "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque reiciendis",
-//   bgColor: "#f42c37",
-// };
+const BannerData = {
+  discount: "30% OFF",
+  title: "Fine Smile",
+  date: "10 Jan to 28 Jan",
+  image: headphone,
+  title2: "Air Solo Bass",
+  title3: "Winter Sale",
+  title4:
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque reiciendis",
+  bgColor: "#f42c37",
+};
 
-// const BannerData2 = {
-//   discount: "30% OFF",
-//   title: "Happy Hours",
-//   date: "14 Jan to 28 Jan",
-//   image: smartwatch2,
-//   title2: "Smart Solo",
-//   title3: "Winter Sale",
-//   title4:
-//     "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque reiciendis",
-//   bgColor: "#2dcc6f",
-// };
+const BannerData2 = {
+  discount: "30% OFF",
+  title: "Happy Hours",
+  date: "14 Jan to 28 Jan",
+  image: smartwatch2,
+  title2: "Smart Solo",
+  title3: "Winter Sale",
+  title4:
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque reiciendis",
+  bgColor: "#2dcc6f",
+};
 
-const App = () => {
+// const App = () => {
   // const [orderPopup, setOrderPopup] = React.useState(false);
 
   // const handleOrderPopup = () => {
@@ -66,13 +66,14 @@ const App = () => {
       <Category2 />
       <Services />
       
-      <Banner  />
+       <Banner data={BannerData2} />
+      <Banner data={BannerData} />
 
       {/* <Hero handleOrderPopup={handleOrderPopup} />
       
     
       <Products />
-      <Banner data={BannerData2} />
+      
       <Blogs />
       <Partners />
       <Footer />
