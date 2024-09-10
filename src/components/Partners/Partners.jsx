94e6841ecd64a1e0ddcb0271cbab3a2dd0a0114d
kashif-aly6466 <159264621+kashif-aly6466@users.noreply.github.com>
2024-09-10 06:47:59 -0700

    fix the bugs

---
+++ src/components/Partners/Partners.jsx
@@ -0,0 +1,14 @@
+// arthemartic operators//
+
+// simple oprators & Uniary oprators
+//simple oprators ma kia hota ha jab ham dakta ha ki kal pahaday usma jo oprators tha un maa doo oprants hota haa or uniary oprators ma surf ak oprant hota ha 
+ //  /'`
+
+// agr ham oprators ma dakha 
+
+
+let a = 2;
+let b = 5 ;
+
+console.log("a=",a,"b=",b);
+
